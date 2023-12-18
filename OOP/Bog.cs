f71697f4023587df1304888c04f45ef383341e1a
SynxEU <126554945+SynxEU@users.noreply.github.com>
2023-12-18 11:46:15 +0100

    Add project files.

---
+++ OOP/Bog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Bog
+    {
+        public string Title { get; set; }
+        public string Forfatter { get; set; }
+        public Bog(string title, string forfatter)
+        {
+            Title = title;
+            Forfatter = forfatter;
+        }
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string title, forfatter;
+            Console.Write("Indtast en bog title: ");
+            title = Console.ReadLine();
+            Console.WriteLine("--------------------------------------");
+            Console.Write("Indtast bogens forfatter: ");
+            forfatter = Console.ReadLine();
+            Bog bog = new Bog(title, forfatter);
+            Console.WriteLine(bog.Title + "\n" + bog.Forfatter);
+        }
+    }
+}
